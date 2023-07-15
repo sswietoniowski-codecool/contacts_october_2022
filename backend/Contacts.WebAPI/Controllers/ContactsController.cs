12ed7d48fc5ed74d4e45e5231e19580a962276cb
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-07-15 13:17:24 +0200

    feat: add get for single contact

--- backend/Contacts.WebAPI/Controllers/ContactsController.cs
+++ backend/Contacts.WebAPI/Controllers/ContactsController.cs
@@ -18,14 +18,39 @@ public class ContactsController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<ContactDto>> GetAll()
     {
-        var contactsDto = _dataService.Contacts.Select(c => new ContactDto()
-        {
-            Id = c.Id,
-            FirstName = c.FirstName,
-            LastName = c.LastName,
-            Email = c.Email
-        });
+        var contactsDto = _dataService.Contacts
+            .Select(c => new ContactDto()
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email
+            });
 
         return Ok(contactsDto);
+    }
+
+    // GET api/contacts/1
+    // GET api/contacts/{id}
+    [HttpGet("{id:int}")]
+    public ActionResult<ContactDto> Get(int id)
+    {
+        var contact = _dataService.Contacts
+            .FirstOrDefault(c => c.Id == id);
+
+        if (contact is null)
+        {
+            return NotFound();
+        }
+
+        var contactDto = new ContactDto()
+        {
+            Id = contact.Id,
+            FirstName = contact.FirstName,
+            LastName = contact.LastName,
+            Email = contact.Email
+        };
+
+        return Ok(contactDto);
     }
 }
