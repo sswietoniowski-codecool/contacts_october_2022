1b7c1bb34fed5a2cb86edec28a9f128885d4a91a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-07-16 09:58:04 +0200

    feat: modify domain classes to use with EF Core

--- backend/Contacts.WebAPI/Domain/Contact.cs
+++ backend/Contacts.WebAPI/Domain/Contact.cs
@@ -1,10 +1,18 @@
-﻿namespace Contacts.WebAPI.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.WebAPI.Domain;
 
 public class Contact
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(32)]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(64)]
     public string LastName { get; set; } = string.Empty;
+    [MaxLength(128)]
     public string Email { get; set; } = string.Empty;
-    public ICollection<Phone> Phones { get; set; } = new List<Phone>();
+    public List<Phone> Phones { get; set; } = new ();
 }
