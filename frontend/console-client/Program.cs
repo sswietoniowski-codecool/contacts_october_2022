fc5e92490bfe03a19fbb71d5d192ee7a6d3579f0
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-07-29 10:56:36 +0200

    feat: add Read action to client

--- frontend/console-client/Program.cs
+++ frontend/console-client/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// CRUD
+
+using System.Text.Json;
+using Contacts.Client.DTOs;
+
+var httpClient = new HttpClient();
+
+Console.WriteLine("GetContacts:\n");
+
+var response = await httpClient.GetAsync("https://localhost:5001/api/contacts");
+
+response.EnsureSuccessStatusCode();
+
+var content = await response.Content.ReadAsStringAsync();
+
+var jsonSerializerOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+var contacts = JsonSerializer.Deserialize<List<ContactDto>>(content, jsonSerializerOptions);
+contacts ??= new List<ContactDto>();
+
+foreach (var contact in contacts)
+{
+    Console.WriteLine($"{contact.Id} {contact.FirstName} {contact.LastName} {contact.Email}");
+}
+
+Console.WriteLine("\nGetContact:\n");
+
+var id = 1;
+response = await httpClient.GetAsync($"https://localhost:5001/api/contacts/{id}");
+
+response.EnsureSuccessStatusCode();
+
+content = await response.Content.ReadAsStringAsync();
+
+var contactDto = JsonSerializer.Deserialize<ContactDetailsDto>(content);
+
+Console.WriteLine($"{contactDto.Id} {contactDto.FirstName} {contactDto.LastName} {contactDto.Email}");
